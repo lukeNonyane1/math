dbd2343687dd406a3098c95cad2714d16991acc0
Luke Nonyane <nonyane18@gmail.com>
2024-04-06 10:43:46 -0400

    added dodoc comments

--- lib.go
+++ lib.go
@@ -1,5 +1,14 @@
 package math_module
 
+// package math_module provides a single utility to calculate the value to two numbers.
+
+// Add calculates the value of summing up two numbers.
+//
+// It has two parameters which are instances of int literal. Add returns the summation of the two int
+// parameters passed in.
+//
+// More information on math can be found at [Math is fun].
+// [Math is fun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
