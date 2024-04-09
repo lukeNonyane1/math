2deccddc554edd50f3d8c594948e95cde2ead505
Luke Nonyane <nonyane18@gmail.com>
2024-04-09 07:58:31 -0400

    added Number interface and made Add a generic

--- lib.go
+++ lib.go
@@ -1,14 +1,15 @@
 package math_module
 
+import "golang.org/x/exp/constraints"
+
 // package math_module provides a single utility to calculate the value to two numbers.
 
-// Add calculates the value of summing up two numbers.
-//
-// It has two parameters which are instances of int literal. Add returns the summation of the two int
-// parameters passed in.
-//
-// More information on math can be found at [Math is fun].
-// [Math is fun]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+// Add is a function that takes in two number values and returns their sum.
+// It follows the rules specified by https://www.mathsisfun.com/numbers/addition.html
+func Add[T Number](a, b T) T {
 	return a + b
+}
+
+type Number interface {
+	constraints.Integer | constraints.Float
 }
